d92646e1323b741890064bb4a8f05eeba19cac9e
Lucas Linhares <lucas.bsb.12@gmail.com>
2022-01-20 11:12:03 -0300

    primeiro commit springboot sem configuracao

---
+++ src/main/java/br/com/desafiosBecaLucasLinhares/BecaDesafiosLucasLinharesApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiosBecaLucasLinhares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecaDesafiosLucasLinharesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecaDesafiosLucasLinharesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafiosBecaLucasLinhares/BecaDesafiosLucasLinharesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiosBecaLucasLinhares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecaDesafiosLucasLinharesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
